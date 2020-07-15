d2bf995b43e8deb54a6da3f4fd68b7d1adbc66b1
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 17:29:18 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,8 +16,8 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
-  def appointments 
-    Appointment.select do |appointment| 
+  def appointments
+    Appointment.select do |appointment|
       appointment.patient == self
     end
   end
