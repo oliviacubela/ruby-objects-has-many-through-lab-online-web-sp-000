f29e436a0604591fd7c13f30ac49f8db4af3e078
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 15:59:17 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,10 +10,10 @@ class Genre
     @artists = artists
     @@all << self
   end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
+  # 
+  # def songs
+  #   Song.all.select {|song| song.artist == self}
+  # end
 
   def self.all
     @@all
