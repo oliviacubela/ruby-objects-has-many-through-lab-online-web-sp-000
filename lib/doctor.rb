e9185c2aee48c0f03525929eaf5b6cba5efde5b9
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 18:05:31 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @appointments = appointments
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+
+  def patients
+    appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre
+  attr_accessor :name, :genre, :artists, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @genre = genre
+    @artists = artists
+    @songs = songs
+    @@all << self
+  end
+  #
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.collect do |song|
+      song.artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
