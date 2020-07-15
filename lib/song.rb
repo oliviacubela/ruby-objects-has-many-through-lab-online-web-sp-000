02d13e9a1b542d0579424a199ad1da48ecb776bc
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 16:20:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,12 @@
 class Song
-  attr_accessor :name, :artist, :genre, :songs
+  attr_accessor :name, :artist, :genre
 
   @@all = []
 
-  def initialize(name, artist, genre, songs)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @songs = songs
     @@all << self
   end
 
