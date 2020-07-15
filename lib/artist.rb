705bd0b3c4300efcc51da8b2d680631c3f48891c
Olivia Cubela <oliviacubela@gmail.com>
2020-07-15 16:21:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  # def new_song(name, genre)
-  #   Song.new(self, name, genre)
-  # end
+  def new_song(name, genre)
+    Song.new(self, name, genre)
+  end
 end
